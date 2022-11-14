8595c87b1243bf9f7a83cf26002dec45a73f402f
pavelzenovich <115029814+pavelzenovich@users.noreply.github.com>
2022-11-14 22:47:10 +0300

    add everything

---
+++ dist/index.bd66c3c0.js
@@ -0,0 +1,9 @@
+vid = document.getElementById("myVideo");
+vid.addEventListener("mouseenter", function() {
+    vid.play();
+});
+vid.addEventListener("mouseleave", function() {
+    vid.pause();
+});
+
+//# sourceMappingURL=index.bd66c3c0.js.map
